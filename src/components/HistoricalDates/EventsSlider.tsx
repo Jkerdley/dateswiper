4f0a5ef58196be772a53fabae66916546aedc1bd
Jkerdley <jk-erley@yandex.ru>
2025-08-04 22:53:45 +0700

    fix: fixes styles for swiper

--- src/components/HistoricalDates/EventsSlider.tsx
+++ src/components/HistoricalDates/EventsSlider.tsx
@@ -132,11 +132,11 @@ export const EventsSlider = ({ events }: EventsSliderProps) => {
 					modules={[Navigation]}
 					// spaceBetween={60}
 					// slidesPerView={3}
-					// observer={true}
-					// observeParents={true}
+					observer={true}
+					observeParents={true}
 					breakpoints={{
-						320: { slidesPerView: 1, spaceBetween: 20 },
-						768: { slidesPerView: 2, spaceBetween: 30 },
+						321: { slidesPerView: 2, spaceBetween: 10 },
+						768: { slidesPerView: 3, spaceBetween: 20 },
 						1024: { slidesPerView: 3, spaceBetween: 40 },
 						// 1440: { slidesPerView: 4, spaceBetween: 60 },
 					}}
